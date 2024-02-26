b19d48380ff6b7f3186e4829c5ea48481519d73f
Ka0sss <enrique.gonzalez.soat@gmail.com>
2024-02-26 20:50:49 -0300

    Se crea sectionUno con su css respectivo|Se borró background-color de root|Se cambia imagen por piratitas

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,21 +1,22 @@
 import "../../assets/style/hero.css";
 import Logo from "../../assets/img/LogoOcean.svg";
-import Secction from "../section/Section"
+import Secction from "../section/Section";
 const Hero = () => {
   return (
     <>
-    <main className="container">
-      <section className="container-hero">
-        <img src={Logo} alt="Logo Ocean Guard" />
-        <div className="ocean">
-          <h1>Ocean Guard </h1>
-          <div className="preservar">
-            <h5>preservando la vida marina</h5>
+      <main className="container">
+        <section className="container-hero">
+          <img src={Logo} alt="Logo Ocean Guard" />
+          <div className="ocean">
+            <h1>Ocean Guard </h1>
+            <div className="preservar">
+              <h5>preservando la vida marina</h5>
+            </div>
           </div>
-        </div>
-      </section>
-    </main>
-    <Secction/>
+        </section>
+      </main>
+      <div className="gradiant"></div>
+      <Secction />
     </>
   );
 };
--- src/components/section/Section.jsx
+++ src/components/section/Section.jsx
@@ -1,29 +1,25 @@
-
-
-
-import "../../assets/style/section.css"
-import Logo from "../../assets/img/LogoOcean.svg"
+import "../../assets/style/section.css";
+import Logo from "../../assets/img/piratitas.webp";
 const Section = () => {
   return (
     <>
-    
-    <section className="sectioncontainer1">
+      <section className="sectioncontainer1">
+        <div className="div-texto">
+          <h1>Preservando la Vida Marina</h1>
+          <p className="primerP">
+            En Ocean Guard, no solo hablamos de preservación,
+          </p>
+          <p className="segundoP">
+            ¡la llevamos a cabo! Nuestra misión es trazar un mapa didáctico de
+            las áreas y especies en veda
+          </p>
+        </div>
+        <div className="div-img">
+          <img src={Logo} alt="" />
+        </div>
+      </section>
+    </>
+  );
+};
 
-    <div className="div-texto">
-    <h1>
-    Preservando la Vida Marina
-    </h1>
-    <p>En Ocean Guard, no solo hablamos de preservación,</p>
-    <p>
-    ¡la llevamos a cabo! Nuestra misión es trazar un mapa didáctico de las áreas y especies en veda
-    </p>
-    </div>
-
-    <div className="div-img">
-    <img src={Logo} alt="" />
-    </div>
-  </section></>
-  )
-}
-
-export default Section
+export default Section;
