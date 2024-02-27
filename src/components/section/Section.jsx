0db700772a3846b6756538b0ba1925ea993fba9a
mauffin <damiancardenas22@gmail.com>
2024-02-27 02:46:52 -0300

    feature: added section in component and img arrow,scroll

--- src/components/section/Section.jsx
+++ src/components/section/Section.jsx
@@ -7,15 +7,32 @@ const Section = () => {
         <div className="div-texto">
           <h1>Preservando la Vida Marina</h1>
           <p className="primerP">
-            En Ocean Guard, no solo hablamos de preservación,
+            En Ocean Guard, no solo 
+            hablamos de preservación,
           </p>
           <p className="segundoP">
-            ¡la llevamos a cabo! Nuestra misión es trazar un mapa didáctico de
+            ¡la llevamos a cabo! Nuestra misión 
+            es trazar un mapa didáctico de
             las áreas y especies en veda
           </p>
         </div>
         <div className="div-img">
           <img src={Logo} alt="" />
+        </div>
+      </section>
+
+      <section className="sectionContainer2">
+        <div className="sectionLeft-text"> 
+          <h1>
+            Preservando la Vida Marina
+          </h1>
+        </div>
+        <div className="secctionRight-text">
+          <p>
+            ¡la llevamos a cabo! Nuestra misión es
+            trazar un mapa didáctico de las áreas
+            y especies en veda
+          </p>
         </div>
       </section>
     </>
