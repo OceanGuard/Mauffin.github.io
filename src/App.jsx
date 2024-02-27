9a684ec676067391a2999a53238655b2de4241d7
mauffin <damiancardenas22@gmail.com>
2024-02-27 10:28:39 -0300

    feature: se agrega dependencia react-router-dom, se crea componente map se importa libreria leaflet

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,21 @@ import Hero from "./components/Hero/Hero"
 import Navbar from "./components/Navbar/nav"
 
 import './App.css'
+import { Router,Route } from "react-router-dom"
 
 function App() {
 
 
   return (
     <div>
-      <Navbar/>
-      <Hero/>
-      
+       <Navbar/>
+      <Router>
+        <Route path="/" element={ <Hero/>}>
+        <Route path="/" element={ <Hero/>}>
+        <Route path="/" element={ <Hero/>}>
+        <Route path="/" element={ <Hero/>}>
+        </Route>
+      </Router>
     </div>
   )
 }
