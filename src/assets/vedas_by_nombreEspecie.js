d3eec8a208a23364284b081a36ac5ee11abbd6cd
sebav <seba.vidal.palacios@gmail.com>
2024-02-27 12:29:00 -0300

    First Leaflet OpenStreetMap working integration, JsonExamples added to assets folder

---
+++ src/assets/vedas_by_nombreEspecie.js
@@ -0,0 +1,324 @@
+let vedas_by_nombreEspecie_Bagrecito = [
+  {
+    vedaId: 50,
+    fechaDeInicio: "2011-10-06T03:00:00.000+00:00",
+    fechaDeTermino: "2026-10-05T03:00:00.000+00:00",
+    descripcionVeda:
+      "Veda extractiva, en las aguas terrestres de todo el territorio nacional,",
+    especificacion: "No sujeto a regulaciones específicas.",
+    especieEnVeda: {
+      especiesId: 5,
+      nombreEspecie: "Bagrecito",
+      nombreCientifico:
+        "Bullockia maldonadoi Trichomycterus chiltoni Trichomycterus  aerolatus Trichomycterus rivulatus Trichomycterus chungaraensis Trichomycterus laucaensis",
+    },
+    regionDeVeda: {
+      regionId: 1,
+      coordenadaX: -20.23989283970564,
+      coordenadaY: -70.13418353488936,
+      nombreRegion: "Región de Tarapacá",
+    },
+  },
+  {
+    vedaId: 51,
+    fechaDeInicio: "2011-10-06T03:00:00.000+00:00",
+    fechaDeTermino: "2026-10-05T03:00:00.000+00:00",
+    descripcionVeda:
+      "Veda extractiva, en las aguas terrestres de todo el territorio nacional,",
+    especificacion: "No sujeto a regulaciones específicas.",
+    especieEnVeda: {
+      especiesId: 5,
+      nombreEspecie: "Bagrecito",
+      nombreCientifico:
+        "Bullockia maldonadoi Trichomycterus chiltoni Trichomycterus  aerolatus Trichomycterus rivulatus Trichomycterus chungaraensis Trichomycterus laucaensis",
+    },
+    regionDeVeda: {
+      regionId: 2,
+      coordenadaX: -23.654658075651685,
+      coordenadaY: -70.40015748520545,
+      nombreRegion: "Región de Antofagasta",
+    },
+  },
+  {
+    vedaId: 52,
+    fechaDeInicio: "2011-10-06T03:00:00.000+00:00",
+    fechaDeTermino: "2026-10-05T03:00:00.000+00:00",
+    descripcionVeda:
+      "Veda extractiva, en las aguas terrestres de todo el territorio nacional,",
+    especificacion: "No sujeto a regulaciones específicas.",
+    especieEnVeda: {
+      especiesId: 5,
+      nombreEspecie: "Bagrecito",
+      nombreCientifico:
+        "Bullockia maldonadoi Trichomycterus chiltoni Trichomycterus  aerolatus Trichomycterus rivulatus Trichomycterus chungaraensis Trichomycterus laucaensis",
+    },
+    regionDeVeda: {
+      regionId: 3,
+      coordenadaX: -27.071400979614264,
+      coordenadaY: -70.80210147647016,
+      nombreRegion: "Región de Atacama",
+    },
+  },
+  {
+    vedaId: 53,
+    fechaDeInicio: "2011-10-06T03:00:00.000+00:00",
+    fechaDeTermino: "2026-10-05T03:00:00.000+00:00",
+    descripcionVeda:
+      "Veda extractiva, en las aguas terrestres de todo el territorio nacional,",
+    especificacion: "No sujeto a regulaciones específicas.",
+    especieEnVeda: {
+      especiesId: 5,
+      nombreEspecie: "Bagrecito",
+      nombreCientifico:
+        "Bullockia maldonadoi Trichomycterus chiltoni Trichomycterus  aerolatus Trichomycterus rivulatus Trichomycterus chungaraensis Trichomycterus laucaensis",
+    },
+    regionDeVeda: {
+      regionId: 4,
+      coordenadaX: -29.895026325939096,
+      coordenadaY: -71.26276833674527,
+      nombreRegion: "Región de Coquimbo",
+    },
+  },
+  {
+    vedaId: 54,
+    fechaDeInicio: "2011-10-06T03:00:00.000+00:00",
+    fechaDeTermino: "2026-10-05T03:00:00.000+00:00",
+    descripcionVeda:
+      "Veda extractiva, en las aguas terrestres de todo el territorio nacional,",
+    especificacion: "No sujeto a regulaciones específicas.",
+    especieEnVeda: {
+      especiesId: 5,
+      nombreEspecie: "Bagrecito",
+      nombreCientifico:
+        "Bullockia maldonadoi Trichomycterus chiltoni Trichomycterus  aerolatus Trichomycterus rivulatus Trichomycterus chungaraensis Trichomycterus laucaensis",
+    },
+    regionDeVeda: {
+      regionId: 5,
+      coordenadaX: -33.016688204648204,
+      coordenadaY: -71.55161984402999,
+      nombreRegion: "Región de Valparaíso",
+    },
+  },
+  {
+    vedaId: 55,
+    fechaDeInicio: "2011-10-06T03:00:00.000+00:00",
+    fechaDeTermino: "2026-10-05T03:00:00.000+00:00",
+    descripcionVeda:
+      "Veda extractiva, en las aguas terrestres de todo el territorio nacional,",
+    especificacion: "No sujeto a regulaciones específicas.",
+    especieEnVeda: {
+      especiesId: 5,
+      nombreEspecie: "Bagrecito",
+      nombreCientifico:
+        "Bullockia maldonadoi Trichomycterus chiltoni Trichomycterus  aerolatus Trichomycterus rivulatus Trichomycterus chungaraensis Trichomycterus laucaensis",
+    },
+    regionDeVeda: {
+      regionId: 6,
+      coordenadaX: -34.37420223332478,
+      coordenadaY: -72.01101256057487,
+      nombreRegion: "Región del Libertador General Bernardo O’Higgins",
+    },
+  },
+  {
+    vedaId: 56,
+    fechaDeInicio: "2011-10-06T03:00:00.000+00:00",
+    fechaDeTermino: "2026-10-05T03:00:00.000+00:00",
+    descripcionVeda:
+      "Veda extractiva, en las aguas terrestres de todo el territorio nacional,",
+    especificacion: "No sujeto a regulaciones específicas.",
+    especieEnVeda: {
+      especiesId: 5,
+      nombreEspecie: "Bagrecito",
+      nombreCientifico:
+        "Bullockia maldonadoi Trichomycterus chiltoni Trichomycterus  aerolatus Trichomycterus rivulatus Trichomycterus chungaraensis Trichomycterus laucaensis",
+    },
+    regionDeVeda: {
+      regionId: 7,
+      coordenadaX: -35.276663704467126,
+      coordenadaY: -72.38407648954171,
+      nombreRegion: "Región del Maule",
+    },
+  },
+  {
+    vedaId: 57,
+    fechaDeInicio: "2011-10-06T03:00:00.000+00:00",
+    fechaDeTermino: "2026-10-05T03:00:00.000+00:00",
+    descripcionVeda:
+      "Veda extractiva, en las aguas terrestres de todo el territorio nacional,",
+    especificacion: "No sujeto a regulaciones específicas.",
+    especieEnVeda: {
+      especiesId: 5,
+      nombreEspecie: "Bagrecito",
+      nombreCientifico:
+        "Bullockia maldonadoi Trichomycterus chiltoni Trichomycterus  aerolatus Trichomycterus rivulatus Trichomycterus chungaraensis Trichomycterus laucaensis",
+    },
+    regionDeVeda: {
+      regionId: 8,
+      coordenadaX: -37.09215002482259,
+      coordenadaY: -73.15441904824428,
+      nombreRegion: "Región del Bio-bío",
+    },
+  },
+  {
+    vedaId: 58,
+    fechaDeInicio: "2011-10-06T03:00:00.000+00:00",
+    fechaDeTermino: "2026-10-05T03:00:00.000+00:00",
+    descripcionVeda:
+      "Veda extractiva, en las aguas terrestres de todo el territorio nacional,",
+    especificacion: "No sujeto a regulaciones específicas.",
+    especieEnVeda: {
+      especiesId: 5,
+      nombreEspecie: "Bagrecito",
+      nombreCientifico:
+        "Bullockia maldonadoi Trichomycterus chiltoni Trichomycterus  aerolatus Trichomycterus rivulatus Trichomycterus chungaraensis Trichomycterus laucaensis",
+    },
+    regionDeVeda: {
+      regionId: 9,
+      coordenadaX: -38.914613579005604,
+      coordenadaY: -73.29415111145599,
+      nombreRegion: "Región de La Araucanía",
+    },
+  },
+  {
+    vedaId: 59,
+    fechaDeInicio: "2011-10-06T03:00:00.000+00:00",
+    fechaDeTermino: "2026-10-05T03:00:00.000+00:00",
+    descripcionVeda:
+      "Veda extractiva, en las aguas terrestres de todo el territorio nacional,",
+    especificacion: "No sujeto a regulaciones específicas.",
+    especieEnVeda: {
+      especiesId: 5,
+      nombreEspecie: "Bagrecito",
+      nombreCientifico:
+        "Bullockia maldonadoi Trichomycterus chiltoni Trichomycterus  aerolatus Trichomycterus rivulatus Trichomycterus chungaraensis Trichomycterus laucaensis",
+    },
+    regionDeVeda: {
+      regionId: 10,
+      coordenadaX: -41.47251563395744,
+      coordenadaY: -72.94313741991519,
+      nombreRegion: "Región de Los Lagos",
+    },
+  },
+  {
+    vedaId: 60,
+    fechaDeInicio: "2011-10-06T03:00:00.000+00:00",
+    fechaDeTermino: "2026-10-05T03:00:00.000+00:00",
+    descripcionVeda:
+      "Veda extractiva, en las aguas terrestres de todo el territorio nacional,",
+    especificacion: "No sujeto a regulaciones específicas.",
+    especieEnVeda: {
+      especiesId: 5,
+      nombreEspecie: "Bagrecito",
+      nombreCientifico:
+        "Bullockia maldonadoi Trichomycterus chiltoni Trichomycterus  aerolatus Trichomycterus rivulatus Trichomycterus chungaraensis Trichomycterus laucaensis",
+    },
+    regionDeVeda: {
+      regionId: 11,
+      coordenadaX: -45.46526437634167,
+      coordenadaY: -72.82102417782455,
+      nombreRegion: "Región Aysén del General Carlos Ibáñez del Campo",
+    },
+  },
+  {
+    vedaId: 61,
+    fechaDeInicio: "2011-10-06T03:00:00.000+00:00",
+    fechaDeTermino: "2026-10-05T03:00:00.000+00:00",
+    descripcionVeda:
+      "Veda extractiva, en las aguas terrestres de todo el territorio nacional,",
+    especificacion: "No sujeto a regulaciones específicas.",
+    especieEnVeda: {
+      especiesId: 5,
+      nombreEspecie: "Bagrecito",
+      nombreCientifico:
+        "Bullockia maldonadoi Trichomycterus chiltoni Trichomycterus  aerolatus Trichomycterus rivulatus Trichomycterus chungaraensis Trichomycterus laucaensis",
+    },
+    regionDeVeda: {
+      regionId: 12,
+      coordenadaX: -51.76847787504497,
+      coordenadaY: -73.37039231990411,
+      nombreRegion: "Región de Magallanes y Antártica Chilena",
+    },
+  },
+  {
+    vedaId: 62,
+    fechaDeInicio: "2011-10-06T03:00:00.000+00:00",
+    fechaDeTermino: "2026-10-05T03:00:00.000+00:00",
+    descripcionVeda:
+      "Veda extractiva, en las aguas terrestres de todo el territorio nacional,",
+    especificacion: "No sujeto a regulaciones específicas.",
+    especieEnVeda: {
+      especiesId: 5,
+      nombreEspecie: "Bagrecito",
+      nombreCientifico:
+        "Bullockia maldonadoi Trichomycterus chiltoni Trichomycterus  aerolatus Trichomycterus rivulatus Trichomycterus chungaraensis Trichomycterus laucaensis",
+    },
+    regionDeVeda: {
+      regionId: 13,
+      coordenadaX: -33.451331109642325,
+      coordenadaY: -70.65484099548475,
+      nombreRegion: "Región Metropolitana de Santiago",
+    },
+  },
+  {
+    vedaId: 63,
+    fechaDeInicio: "2011-10-06T03:00:00.000+00:00",
+    fechaDeTermino: "2026-10-05T03:00:00.000+00:00",
+    descripcionVeda:
+      "Veda extractiva, en las aguas terrestres de todo el territorio nacional,",
+    especificacion: "No sujeto a regulaciones específicas.",
+    especieEnVeda: {
+      especiesId: 5,
+      nombreEspecie: "Bagrecito",
+      nombreCientifico:
+        "Bullockia maldonadoi Trichomycterus chiltoni Trichomycterus  aerolatus Trichomycterus rivulatus Trichomycterus chungaraensis Trichomycterus laucaensis",
+    },
+    regionDeVeda: {
+      regionId: 14,
+      coordenadaX: -39.81429712398237,
+      coordenadaY: -73.25795428945176,
+      nombreRegion: "Región de Los Ríos",
+    },
+  },
+  {
+    vedaId: 64,
+    fechaDeInicio: "2011-10-06T03:00:00.000+00:00",
+    fechaDeTermino: "2026-10-05T03:00:00.000+00:00",
+    descripcionVeda:
+      "Veda extractiva, en las aguas terrestres de todo el territorio nacional,",
+    especificacion: "No sujeto a regulaciones específicas.",
+    especieEnVeda: {
+      especiesId: 5,
+      nombreEspecie: "Bagrecito",
+      nombreCientifico:
+        "Bullockia maldonadoi Trichomycterus chiltoni Trichomycterus  aerolatus Trichomycterus rivulatus Trichomycterus chungaraensis Trichomycterus laucaensis",
+    },
+    regionDeVeda: {
+      regionId: 15,
+      coordenadaX: -18.456376101184354,
+      coordenadaY: -70.32209176597149,
+      nombreRegion: "Región de Arica y Parinacota",
+    },
+  },
+  {
+    vedaId: 65,
+    fechaDeInicio: "2011-10-06T03:00:00.000+00:00",
+    fechaDeTermino: "2026-10-05T03:00:00.000+00:00",
+    descripcionVeda:
+      "Veda extractiva, en las aguas terrestres de todo el territorio nacional,",
+    especificacion: "No sujeto a regulaciones específicas.",
+    especieEnVeda: {
+      especiesId: 5,
+      nombreEspecie: "Bagrecito",
+      nombreCientifico:
+        "Bullockia maldonadoi Trichomycterus chiltoni Trichomycterus  aerolatus Trichomycterus rivulatus Trichomycterus chungaraensis Trichomycterus laucaensis",
+    },
+    regionDeVeda: {
+      regionId: 16,
+      coordenadaX: -36.12679977635618,
+      coordenadaY: -72.81683802433277,
+      nombreRegion: "Región de Ñuble",
+    },
+  },
+];
+
+export default vedas_by_nombreEspecie_Bagrecito;
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,24 +1,23 @@
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "../../assets/style/map.css";
-import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+import "leaflet/dist/leaflet.css";
 
+let marker = [-20.23989283970564, -70.13418353488936];
 
 const Map = () => {
   return (
     <>
-
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
+      <MapContainer center={marker} zoom={13}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[-20.23989283970564, -70.13418353488936]}>
+          <Popup>Region de Tarapacá</Popup>
+        </Marker>
+      </MapContainer>
     </>
-  )
-}
+  );
+};
 
-export default Map
+export default Map;
