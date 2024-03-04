84d8419089d4564482f4eca1a524db459db36175
Damian Cardenas <damiancardenas22@gmail.com>
2024-03-04 15:10:36 -0300

    se consumen los datos de regiones mediante axios

--- src/components/Lista/ListaRegion.jsx
+++ src/components/Lista/ListaRegion.jsx
@@ -1,12 +1,29 @@
-import data from "../../assets/regiones_by_id_tarapaca";
-import { Link } from "react-router-dom";
+import { useEffect,useState } from "react";
+import { Link} from "react-router-dom";
+import axios from "axios";
 
 function ListaRegion({ setMarkerPosition }) {
+
+const [lista,setLista]= useState([]);
+
+
+useEffect(()=>{
+ axios.get("http://localhost:8080/api/region/regiones")
+  .then((response)=>{
+    setLista(response.data)
+    console.log(response.data)
+  } )
+   .catch((error)=>{
+    console.log("error al cosumir los datos de las regiones" + error)
+  })
+ 
+},[])
+
   return (
     <>
       <ul>
-        {data.map((item) => (
-          <li key={item.regionId}>
+        {lista.map((item,index) => (
+          <li key={index}>
             <Link
               to={`/map/especies/${item.regionId}`}
               onClick={() =>
