6870a3ad57fd1230783b1dcd758d66f8b7d7df53
JaviBLL <javiera.lagos3108@gmail.com>
2024-03-05 17:11:26 -0300

    reponsive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,37 +9,41 @@ import { Link } from 'react-router-dom'
 
 
 function Footer() {
-    return (
-        <footer className="footer" >
+  return (
+    <footer className="footer" >
 
-                <section className='columnas'>
-                    <section className="menu">
-                        <h6 className='titulo-menu'>Menu</h6>
-                        <ul>
-                            <li><Link to="/">Inicio</Link></li>
-                            <li><Link to="/sobre-nosotros">Sobre nosotros</Link></li>
-                            <li><Link to="/mapa">Mapa</Link></li>
-                        </ul>
-                    </section>
-                    <section className="contacto">
-                        <h6 className='titulo-contacto'>Contacto</h6>
-                        <ul>
-                            <li><img src={localizacion} alt="icono-localizacion" /><span>Santiago, Chile</span></li>
-                            <li><img className="numero" src={numero} alt="icono-telefono" /><span>+56955555555</span></li>
-                            <li><img src={mail} alt="icono-mail" /> <span>mail@mail.com</span></li>
-                        </ul>
-                    </section>
-                    <section className="logo">
-                        <img src={logoFooter} alt="logo-OcenGuard" />
-                    </section>
-                </section>
+      <section className='columnas'>
+        <section className="menu">
+          <h6 className='titulo-menu'>Menu</h6>
+          <ul>
+            <li><Link to="/">Inicio</Link></li>
+            <li><Link to="/sobre-nosotros">Sobre nosotros</Link></li>
+            <li><Link to="/mapa">Mapa</Link></li>
+          </ul>
+        </section>
+        <section className="contacto">
+          <h6 className='titulo-contacto'>Contacto</h6>
+          <ul>
+            <li><img src={localizacion} alt="icono-localizacion" /><span>Santiago, Chile</span></li>
+            <li><img className="numero" src={numero} alt="icono-telefono" /><span>+56955555555</span></li>
+            <li><img src={mail} alt="icono-mail" /> <span>mail@mail.com</span></li>
+          </ul>
+        </section>
+        <section className="logo">
+          <div className='contenedor-imagen-logo'>
+          <img src={logoFooter} alt="logo-OcenGuard" />
+          </div>
+        </section>
+      </section>
 
-                <section className='frase-final'>
-                <img src={corazon} alt="icono-corazon" />
-                <p>Hecho con amor por OceanGuards</p>
-            </section>
-        </footer>
-    )
+      <section className='frase-final'>
+        <div className='imagen-corazon'>
+          <img src={corazon} alt="icono-corazon" />
+        </div>
+        <p>Hecho con amor por OceanGuards</p>
+      </section>
+    </footer>
+  )
 };
 
 export default Footer;
