02f3ba4814544ff9adc8e72388e9bcc51c0031b5
Ka0sss <enrique.gonzalez.soat@gmail.com>
2024-03-06 11:37:38 -0300

    Se hace responsive navbar y se agrega menu desplegable al responsive

--- src/components/Navbar/nav.jsx
+++ src/components/Navbar/nav.jsx
@@ -1,25 +1,72 @@
-import "../../assets/style/navbar.css";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import logoNav from "../../assets/img/icons/LogoOcean16x16.svg";
+import "../../assets/style/navbar.css";
 
-/* cambiar etiquetas a por link cuando se agregen las dependencias react router */
-const nav = () => {
+const Nav = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const openNav = () => {
+    setIsMobileMenuOpen(true);
+  };
+
+  const closeNav = () => {
+    setIsMobileMenuOpen(false);
+  };
+
+  const handleLinkClick = () => {
+    closeNav();
+  };
+
   return (
     <>
       <nav className="navbar">
+        <div className="logoNav">
+          <img src={logoNav} alt="logoNav" />
+        </div>
         <ul>
           <li>
-            <Link to="/">Inicio</Link>
+            <Link to="/" onClick={handleLinkClick}>
+              Inicio
+            </Link>
           </li>
           <li>
-            <Link to="/map">Mapa</Link>
+            <Link to="/map" onClick={handleLinkClick}>
+              Mapa
+            </Link>
           </li>
           <li>
-            <Link to="/sobre-nosotros">Sobre Nosotros</Link>
+            <Link to="/sobre-nosotros" onClick={handleLinkClick}>
+              Sobre Nosotros
+            </Link>
           </li>
         </ul>
+        <div></div>
+        <a onClick={openNav} className="menuNav" href="#">
+          <button>Menu</button>
+        </a>
+        <div
+          className={`overlay ${isMobileMenuOpen ? "open" : ""}`}
+          id="mobile-menu"
+        >
+          <a onClick={closeNav} className="closeNav" href="#">
+            &times;
+          </a>
+          <div className="overlay-content">
+            <Link to="/" onClick={handleLinkClick}>
+              Inicio
+            </Link>
+            <Link to="/map" onClick={handleLinkClick}>
+              Mapa
+            </Link>
+            <Link to="/sobre-nosotros" onClick={handleLinkClick}>
+              Sobre Nosotros
+            </Link>
+          </div>
+        </div>
       </nav>
     </>
   );
 };
 
-export default nav;
+export default Nav;
