809c7ffe8b6ef7db4eadbac25b0c50ff00b536ea
Be-make <bailoyola.silva@gmail.com>
2024-03-06 10:33:46 -0300

    se crea botón de inicio estatico

---
+++ src/components/BotonInicio/BotonInicio.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import '../../assets/style/BotonInicio.css';
+
+const BotonInicio = () => {
+    const [isVisible, setIsVisible] = useState(false);
+  
+    useEffect(() => {
+      window.addEventListener('scroll', handleScroll);
+      return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+  
+    const handleScroll = () => {
+      if (window.pageYOffset > 100) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+  
+    const scrollToTop = () => {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+    };
+  
+    return (
+      <button 
+        className={`boton-inicio ${isVisible ? 'visible' : 'hidden'}`}
+        onClick={scrollToTop}
+      >
+        Inicio
+      </button>
+    );
+  };
+
+  export default BotonInicio;
