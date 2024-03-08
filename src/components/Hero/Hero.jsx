f35d6b4c0f1e6b6fb15236c0861ad0885782b203
Be-make <bailoyola.silva@gmail.com>
2024-03-08 00:33:29 -0300

    Se crea animación para el logo principal

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,4 +1,5 @@
 import "../../assets/style/hero.css";
+import gsap from "gsap";
 import Logo from "../../assets/img/LogoOcean.svg";
 {
   /*import Arrow from "../../assets/img/arrow-scroll.svg"*/
@@ -8,6 +9,19 @@ import Secction from "../section/Section";
 import Informacion from "../Informacion/Informacion";
 
 const Hero = () => {
+
+  useEffect(() => {
+    gsap.to("#path1", { duration: 1, opacity: 1, y: 0 });
+    gsap.to("#path2", { duration: 1, opacity: 1, y: 0, delay: 0.2 });
+    gsap.to("#path3", { duration: 1, opacity: 1, y: 0, delay: 0.4 });
+    gsap.to("#path4", { duration: 1, opacity: 1, y: 0, delay: 0.6 });
+    gsap.to("#path5", { duration: 1, opacity: 1, y: 0, delay: 0.8 });
+    gsap.to("#path6", { duration: 1, opacity: 1, y: 0, delay: 1 });
+    gsap.to("#ellipse1", { duration: 1, opacity: 1, y: 0, delay: 1.2 });
+    gsap.to("#path7", { duration: 1, opacity: 1, y: 0, delay: 1.4 });
+    gsap.to("#path8", { duration: 1, opacity: 1, y: 0, delay: 1.6 });
+  }, []);
+  
   return (
     <>
       <main className="container">
