691f1654c6571544f7c8895ba95b24015012ff17
Damian Cardenas <damiancardenas22@gmail.com>
2024-03-21 02:04:08 -0300

    fixed

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://Mauffin.github.io/frontend"
+  base:"https://oceanguard.github.io/frontend/"
 })
 
